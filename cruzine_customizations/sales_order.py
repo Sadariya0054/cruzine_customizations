481abc5dd4be17e5188722b684af4de31df8f315
Jigar Tarpara <jigartarpara@khatavahi.in>
2024-04-30 11:38:22 +0530

    Cruzine Customizations

--- cruzine_customizations/sales_order.py
+++ cruzine_customizations/sales_order.py
@@ -2,25 +2,19 @@ import frappe
 
 @frappe.whitelist()
 def get_shipping_rules(doctype, txt, searchfield, start, page_len, filters):
-	qe = ' and cmt.city_master="'+filters['city']+'"'
-	table = ""
-	if filters.get('incoterm',''):
-		qe = ' and sr.name = imt.parent and imt.incoterm="'+filters['incoterm']+'"'
-		table = '''`tabCity Master Table` as cmt,
-			`tabIncoterm Master Table` as imt'''
-	else:
-		table = '''`tabCity Master Table` as cmt'''
+	qe = ' and cmt.city_master="'+filters.get('city')+ '"  and imt.incoterm="'+filters.get('incoterm')+'"'
 	address = frappe.db.sql(""" 
         select 
             sr.name 
         from 
             `tabShipping Rule` as sr,
-            %s
+            `tabCity Master Table` as cmt,
+			`tabIncoterm Master Table` as imt
         where 
             sr.name = cmt.parent 
-            
+			and sr.name = imt.parent   
             %s
         group by
             sr.name
-    """%(table,qe))
+    """%(qe))
 	return address
