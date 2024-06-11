a36c2635ec4cd6333eb82dbcc5044932e2241344
Jigar Tarpara <jigartarpara@khatavahi.in>
2024-06-11 15:43:17 +0530

    Chages

--- cruzine_customizations/customer.py
+++ cruzine_customizations/customer.py
@@ -1,8 +1,10 @@
 import frappe
 
 def capitalize(sentence):
-    return (' ').join(map(lambda word: word[0].upper() + word[1:], sentence.split(' ')))
-
+    try:
+        return (' ').join(map(lambda word: word[0].upper() + word[1:], sentence.split(' ')))
+    except:
+        return sentence
 
 def validate(doc, method):
     doc.customer_name = capitalize(doc.customer_name)
